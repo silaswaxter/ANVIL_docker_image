3e01c4d3fa4ed7d491a05d1009739fe5a8c99b28
Silas Waxter <silaswaxter@gmail.com>
2022-04-16 23:39:27 -0700

    Curl_Functions.sh follows global SILENT_FLAG
    Added conditional logic to curl function that passes the silent flag to
    curl when SILENT_FLAG!=0.  Otherwise, outputs normal progress bar.

--- anvil/install_tools/curl_functions.sh
+++ anvil/install_tools/curl_functions.sh
@@ -25,21 +25,26 @@ then
     # FUNCTION:	Downloads a file with curl
     # ARG1:		output file name
     # ARG2:		url to download from
-    # ARG3:		flags
+    # ARG3:		curl flags
     download_with_curl () {
+        # convert SILENT_FLAG to curl silent flag
+        local conditional_curl_silent_flag=""
+        SILENT_FLAG=${SILENT_FLAG:=0}
+        if [ ! ${SILENT_FLAG} -eq 0 ]
+        then
+            conditional_curl_silent_flag="-s"
+        fi
+
         if test "$3" == ""; then
-            curl -fL -A "Chrome" -o "$1" "$2"
+            curl -fL -A "Chrome" ${conditional_curl_silent_flag} -o "$1" "$2"
         else
-            curl -fL -A "Chrome" -o "$1" "$3" "$2"
+            curl -fL -A "Chrome" ${conditional_curl_silent_flag} -o "$1" "$3" "$2"
         fi
 
         #Error Handling
-        if test "$?" == "0"; then
-            print_conditionally "\t success.\n"
-
-        else
+        if test "$?" != "0"; then
             # always print fail message
-            echo -e "\t curl command failed with: $?"
+            echo -e "\t ERROR: curl command failed with: $?"
             echo -e "\t\t [TIP]: user running script needs read/write access to output dir"
             echo -e "\t exiting...\n"
             exit $?
